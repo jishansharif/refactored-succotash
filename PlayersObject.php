eef71b65c4579934595d236d11d0ecc3870c7737
Jishan Sharif <jishan@medsender.com>
2022-01-30 14:25:17 -0500

    Refactored getPlayerFromArray method and added new extract data method

--- PlayersObject.php
+++ PlayersObject.php
@@ -108,37 +108,7 @@ class PlayersObject implements IReadWritePlayers {
             $jonas->salary = $playerData['salary'];
             $players[] = $data;
         }
-
-        // $jonas = new \stdClass();
-        // $jonas->name = 'Jonas Valenciunas';
-        // $jonas->age = 26;
-        // $jonas->job = 'Center';
-        // $jonas->salary = '4.66m';
-        // $players[] = $jonas;
-
-        // $kyle = new \stdClass();
-        // $kyle->name = 'Kyle Lowry';
-        // $kyle->age = 32;
-        // $kyle->job = 'Point Guard';
-        // $kyle->salary = '28.7m';
-        // $players[] = $kyle;
-
-        // $demar = new \stdClass();
-        // $demar->name = 'Demar DeRozan';
-        // $demar->age = 28;
-        // $demar->job = 'Shooting Guard';
-        // $demar->salary = '26.54m';
-        // $players[] = $demar;
-
-        // $jakob = new \stdClass();
-        // $jakob->name = 'Jakob Poeltl';
-        // $jakob->age = 22;
-        // $jakob->job = 'Center';
-        // $jakob->salary = '2.704m';
-        // $players[] = $jakob;
-
         return $players;
-
     }
 
     function extractFileContent($filename=null) {
